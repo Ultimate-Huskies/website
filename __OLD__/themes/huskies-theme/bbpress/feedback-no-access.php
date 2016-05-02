cfdbb5420d18a17bebc094bf044275273dc07314
Mupat <dev@mupat.net>
2016-05-02 17:16:31 +0200

    remove old stuff

--- __OLD__/themes/huskies-theme/bbpress/feedback-no-access.php
+++
@@ -1,4 +0,0 @@
-<div id="forum-private" class="alert alert-block">
-	<h1 class="entry-title"><?php _e('Private', 'huskies-theme'); ?></h1>
-	<p><?php _e('You do not have permission to view this forum.', 'huskies-theme'); ?></p>
-</div>
