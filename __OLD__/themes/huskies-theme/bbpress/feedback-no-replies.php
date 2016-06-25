3f71d283f570c5eb11fd286ea26bac7893c3dc07
Mupat <dev@mupat.net>
2016-06-25 10:07:01 +0200

    remove old stuff

--- __OLD__/themes/huskies-theme/bbpress/feedback-no-replies.php
+++
@@ -1,3 +0,0 @@
-<div class="alert alert-info">
-	<?php _e('Oh bother! No replies were found here!', 'huskies-theme'); ?>
-</div>
--- __OLD__/themes/huskies-theme/bbpress/feedback-no-topics.php
+++
@@ -1,3 +0,0 @@
-<div class="alert alert-info">
-  <h3><?php _e('No topics found!', 'huskies-theme'); ?></h3>
-</div>
